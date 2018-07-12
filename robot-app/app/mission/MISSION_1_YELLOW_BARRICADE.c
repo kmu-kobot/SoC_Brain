959ca18812033cf2ef643fd78f51342a86c7b348
wlsdudqo97 <31851634+wlsdudqo97@users.noreply.github.com>
2018-07-12 19:55:11 +0900

    feat: 로그 출력 기능 추가

--- robot-app/app/mission/MISSION_1_YELLOW_BARRICADE.c
+++ robot-app/app/mission/MISSION_1_YELLOW_BARRICADE.c
@@ -15,7 +15,8 @@ int mission_1_1_wait_yellow_barricade(U16 *image) {
             );
         }
     }
-
+    
+    printf("M1-1: WAIT: %f", cntYellow * 2 * 100 / (WIDTH * MISSION_1_RANGE));
     return cntYellow * 2 * 100 / (WIDTH * MISSION_1_RANGE) > MISSION_1_THRESHOLDS;
 }
 
@@ -30,8 +31,9 @@ int mission_1_2_end_yellow_barricade(U16 *image) {
             );
         }
     }
-
-    return cntYellow * 2 * 100 / (WIDTH * HEIGHT) <= 10;
+    
+    printf("M1-2: END: %f", cntYellow * 2 * 100 / (WIDTH * HEIGHT));
+    return cntYellow * 2 * 100 / (WIDTH * HEIGHT) <= (MISSION_1_THRESHOLDS - 20);
 }
 
 void mission_1_3_escape_yellow_barricade(int repeat) {
