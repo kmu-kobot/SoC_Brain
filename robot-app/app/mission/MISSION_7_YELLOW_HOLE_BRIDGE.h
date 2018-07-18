e9fddbf1ad5f1c946bde22547ca90c0c79473f9d
wlsdudqo97 <31851634+wlsdudqo97@users.noreply.github.com>
2018-07-18 22:06:57 +0900

    feat: MISSION_7_YELLOW_BRIDGE.h 구현

--- robot-app/app/mission/MISSION_7_YELLOW_HOLE_BRIDGE.h
+++ robot-app/app/mission/MISSION_7_YELLOW_HOLE_BRIDGE.h
@@ -15,23 +15,34 @@
 #define MISSION_7_2_BLACK_LINE_COL_POINT_1 70
 #define MISSION_7_2_BLACK_LINE_COL_POINT_2 130
 
-#define MISSION_7_2_RED_BRIDGE_THRESHOLDS 10
+#define MISSION_7_2_RED_BRIDGE_THRESHOLDS 7
+
+#define MISSION_7_4_YELLOW_BRIDGE_SLOPE 5
+#define MISSION_7_4_YELLOW_BRIDGE_SLOPE_ERROR 3
+
+#define MISSION_7_4_COL_POINT_1 100
+#define MISSION_7_4_COL_POINT_2 140
+
+
+//TODO: 거리에 짤게 걷기, 길게 걷기 수정 
+#define MISSION_7_4_YELLOW_LENGTH 18
+#define MISSION_7_4_YELLOW_LENGTH_ERROR 5
 
 #define MISSION_7_4_BLACK_RATIO 10
 #define MISSION_7_4_BLACK_RATIO_ERROR 5
 #define MISSION_7_4_HEIGHT_SCOPE 60
 
 #define MISSION_7_5_BLACK_LINE_RANGE 5
-#define MISSION_7_5_BLACK_LINE_ERROR 2
+#define MISSION_7_5_BLACK_LINE_ERROR 3
 
 #define MISSION_7_5_YELLOW_BRIDGE_POINT_Y_1 30
-#define MISSION_7_5_YELLOW_BRIDGE_POINT_Y_2 50
+#define MISSION_7_5_YELLOW_BRIDGE_POINT_Y_2 40
 #define MISSION_7_5_YELLOW_BRIDGE_SLOPE_RANGE 7
-#define MISSION_7_5_YELLOW_BRIDGE_SLOPE 5
+#define MISSION_7_5_YELLOW_BRIDGE_SLOPE 0
 
 #define MISSION_7_5_HEIGHT_START 80
 #define MISSION_7_5_HEIGHT_RANGE 15
-#define MISSION_7_5_LINE_RATIO 70
+#define MISSION_7_5_LINE_RATIO 71
 #define MISSION_7_5_LINE_RATIO_ERROR 5
 
 #define MISSION_7_7_BLACK_LINE_SLOPE 7.5
@@ -46,6 +57,10 @@ void mission_7_2_watch_below(void);
 int mission_7_2_before_bridge_set_center(U16 *image);
 
 int mission_7_3_climb_yellow_hole_bridge(void);
+
+int mission_7_4_set_center(U16* image);
+
+int mission_7_4_set_straight(U16* image);
 
 void mission_7_4_watch_below(int repeat);
 
@@ -68,4 +83,5 @@ int mission_7_7_after_yellow_bridge_set_straight(U16 *image);
 int mission_7_7_after_yellow_bridge_set_center(U16 *image);
 
 #endif //SOC_APP_MISSION_7_YELLOW_HOLE_BRIDGE_H
+
 
