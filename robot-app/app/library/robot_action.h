be6433e02de1f14bc978062ec6e1baaa922bdd33
Gihyeon Yang <dexterastin@gmail.com>
2018-07-23 19:28:31 +0900

    update: robot action library

--- robot-app/app/library/robot_action.h
+++ robot-app/app/library/robot_action.h
@@ -8,8 +8,6 @@
 
 #define SOC_APP_ROBOT_ACTION_H
 
-#define
-
 #define INIT_POSE_COEF 5
 #define INIT_VIEW_COEF 1
 #define WALK_STEP_COEF 1
@@ -18,16 +16,19 @@
 #define TURN_DIR_COEF 1
 #define TURN_POSE_COEF 15
 #define TURN_VIEW_COEF 3
+#define MOVE_LEN_COEF 33
 #define MOVE_DIR_COEF 1
 #define MOVE_POSE_COEF 11
 #define MOVE_VIEW_COEF 2
+#define BIT_DIR_COEF 5
 
 #define INIT_MOTION(pose, view) (INIT_LOW_DOWN + INIT_POSE_COEF*pose + INIT_VIEW_COEF*view)
 #define WALK_START_MOTION(speed, view) (WALK_FAST_START_DOWN + WALK_SPEED_COEF*speed + WALK_VIEW_COEF*view)
 #define WALK_END_MOTION(speed, view) (WALK_FAST_END_DOWN + WALK_SPEED_COEF*speed + WALK_VIEW_COEF*view)
 #define WALK_MOTION(step, speed, view) (WALK_FAST_L_DOWN + WALK_STEP_COEF*step + WALK_SPEED_COEF*speed + WALK_VIEW_COEF*view)
 #define TURN_MOTION(dir, pose, view) (TURN_LEFT_LOW_DOWN + TURN_DIR_COEF*dir + TURN_POSE_COEF*pose + TURN_VIEW_COEF*view)
-#define MOVE_MOTION(dir, pose, view) (MOVE_LEFT_LOW_DOWN + MOVE_DIR_COEF*dir + MOVE_POSE_COEF*pose + MOVE_VIEW_COEF*view)
+#define MOVE_MOTION(len, dir, pose, view) (LONG_MOVE_LEFT_LOW_DOWN + MOVE_LEN_COEF*len + MOVE_DIR_COEF*dir + MOVE_POSE_COEF*pose + MOVE_VIEW_COEF*view)
+#define BIT_MOTION(dir) (BIT_FRONT + BIT_DIR_COEF*dir)
 
 typedef enum {
     INIT_LOW_DOWN = 1,
@@ -90,63 +91,91 @@ typedef enum {
     TURN_LEFT_LOW_RIGHT = 59,
     TURN_RIGHT_LOW_RIGHT,
 
-    TURN_LEFT_HIGH_DOWN = 62,
+    TURN_LEFT_HIGH_DOWN = 77,
     TURN_RIGHT_HIGH_DOWN,
 
-    TURN_LEFT_HIGH_OBLIQUE = 65,
+    TURN_LEFT_HIGH_OBLIQUE = 80,
     TURN_RIGHT_HIGH_OBLIQUE,
 
-    TURN_LEFT_HIGH_LEFT = 71,
+    TURN_LEFT_HIGH_LEFT = 86,
     TURN_RIGHT_HIGH_LEFT,
 
-    TURN_LEFT_HIGH_RIGHT = 74,
+    TURN_LEFT_HIGH_RIGHT = 89,
     TURN_RIGHT_HIGH_RIGHT,
 
-    MOVE_LEFT_LOW_DOWN = 78,
-    MOVE_RIGHT_LOW_DOWN,
-    MOVE_LEFT_LOW_OBLIQUE,
-    MOVE_RIGHT_LOW_OBLIQUE,
-    MOVE_LEFT_LOW_LEFT = 84,
-    MOVE_RIGHT_LOW_LEFT,
-    MOVE_LEFT_LOW_RIGHT,
-    MOVE_RIGHT_LOW_RIGHT,
+    LONG_MOVE_LEFT_LOW_DOWN = 93,
+    LONG_MOVE_RIGHT_LOW_DOWN,
+    LONG_MOVE_LEFT_LOW_OBLIQUE,
+    LONG_MOVE_RIGHT_LOW_OBLIQUE,
+    LONG_MOVE_LEFT_LOW_LEFT = 99,
+    LONG_MOVE_RIGHT_LOW_LEFT,
+    LONG_MOVE_LEFT_LOW_RIGHT,
+    LONG_MOVE_RIGHT_LOW_RIGHT,
 
-    MOVE_LEFT_MIDDLE_DOWN = 89,
-    MOVE_RIGHT_MIDDLE_DOWN,
-    MOVE_LEFT_MIDDLE_OBLIQUE,
-    MOVE_RIGHT_MIDDLE_OBLIQUE,
-    MOVE_LEFT_MIDDLE_LEFT = 95,
-    MOVE_RIGHT_MIDDLE_LEFT,
-    MOVE_LEFT_MIDDLE_RIGHT,
-    MOVE_RIGHT_MIDDLE_RIGHT,
+    LONG_MOVE_LEFT_MIDDLE_DOWN = 104,
+    LONG_MOVE_RIGHT_MIDDLE_DOWN,
+    LONG_MOVE_LEFT_MIDDLE_OBLIQUE,
+    LONG_MOVE_RIGHT_MIDDLE_OBLIQUE,
+    LONG_MOVE_LEFT_MIDDLE_LEFT = 110,
+    LONG_MOVE_RIGHT_MIDDLE_LEFT,
+    LONG_MOVE_LEFT_MIDDLE_RIGHT,
+    LONG_MOVE_RIGHT_MIDDLE_RIGHT,
 
-    MOVE_LEFT_HIGH_DOWN = 100,
-    MOVE_RIGHT_HIGH_DOWN,
-    MOVE_LEFT_HIGH_OBLIQUE,
-    MOVE_RIGHT_HIGH_OBLIQUE,
-    MOVE_LEFT_HIGH_LEFT = 106,
-    MOVE_RIGHT_HIGH_LEFT,
-    MOVE_LEFT_HIGH_RIGHT,
-    MOVE_RIGHT_HIGH_RIGHT,
+    LONG_MOVE_LEFT_HIGH_DOWN = 115,
+    LONG_MOVE_RIGHT_HIGH_DOWN,
+    LONG_MOVE_LEFT_HIGH_OBLIQUE,
+    LONG_MOVE_RIGHT_HIGH_OBLIQUE,
+    LONG_MOVE_LEFT_HIGH_LEFT = 121,
+    LONG_MOVE_RIGHT_HIGH_LEFT,
+    LONG_MOVE_LEFT_HIGH_RIGHT,
+    LONG_MOVE_RIGHT_HIGH_RIGHT,
 
-    MISSION_1_RED_DUMBLING = 162,
+    SHORT_MOVE_LEFT_LOW_DOWN = 126,
+    SHORT_MOVE_RIGHT_LOW_DOWN,
+    SHORT_MOVE_LEFT_LOW_OBLIQUE,
+    SHORT_MOVE_RIGHT_LOW_OBLIQUE,
+    SHORT_MOVE_LEFT_LOW_LEFT = 132,
+    SHORT_MOVE_RIGHT_LOW_LEFT,
+    SHORT_MOVE_LEFT_LOW_RIGHT,
+    SHORT_MOVE_RIGHT_LOW_RIGHT,
 
-    MISSION_2_MINE_WALK_START = 167,
-    MISSION_2_MINE_WALK_L,
-    MISSION_2_MINE_WLAK_R,
-    MISSION_2_MINE_WALK_END,
+    SHORT_MOVE_LEFT_MIDDLE_DOWN = 137,
+    SHORT_MOVE_RIGHT_MIDDLE_DOWN,
+    SHORT_MOVE_LEFT_MIDDLE_OBLIQUE,
+    SHORT_MOVE_RIGHT_MIDDLE_OBLIQUE,
+    SHORT_MOVE_LEFT_MIDDLE_LEFT = 143,
+    SHORT_MOVE_RIGHT_MIDDLE_LEFT,
+    SHORT_MOVE_LEFT_MIDDLE_RIGHT,
+    SHORT_MOVE_RIGHT_MIDDLE_RIGHT,
 
-    MISSION_3_HURDLING = 173,
+    SHORT_MOVE_LEFT_HIGH_DOWN = 148,
+    SHORT_MOVE_RIGHT_HIGH_DOWN,
+    SHORT_MOVE_LEFT_HIGH_OBLIQUE,
+    SHORT_MOVE_RIGHT_HIGH_OBLIQUE,
+    SHORT_MOVE_LEFT_HIGH_LEFT = 154,
+    SHORT_MOVE_RIGHT_HIGH_LEFT,
+    SHORT_MOVE_LEFT_HIGH_RIGHT,
+    SHORT_MOVE_RIGHT_HIGH_RIGHT,
 
-    MISSION_5_STAIR_UP = 177,
+    BIT_FRONT = 159,
 
-    MISSION_5_STAIR_DOWN = 180,
+    BIT_BACK = 164,
 
-    MISSION_6_RIGHT_KICK = 185,
+    MISSION_2_RED_DUMBLING = 177,
 
-    MISSION_7_YELLOW_DUMBLING = 188,
+    MISSION_3_MINE_WALK = 182,
 
-    TAKE_REST = 193,
+    MISSION_4_HURDLING = 188,
+
+    MISSION_5_STAIR_UP = 192,
+
+    MISSION_5_STAIR_DOWN = 195,
+
+    MISSION_6_RIGHT_KICK = 200,
+
+    MISSION_7_YELLOW_DUMBLING = 203,
+
+    TAKE_REST = 208,
 
     NIL = 0xff
 } MOTION;
@@ -203,6 +232,11 @@ typedef enum {
 } DIRECTION;
 
 typedef enum {
+    LONG = 0,
+    SHORT,
+} LENGTH;
+
+typedef enum {
     CHECK = 0,
     SET
 } FOO_MOD;
@@ -220,6 +254,7 @@ static inline void action(MOTION_INIT init, MOTION motion) {
 
 static inline void ACTION_INIT(POSE pose, VIEW view) {
     RobotAction(INIT_MOTION(pose, view));
+    foo(INIT_MOTION(pose, view), SET);
 }
 
 
@@ -230,7 +265,7 @@ static inline void ACTION_INIT(POSE pose, VIEW view) {
 static inline void ACTION_WALK(SPEED speed, VIEW view, int repeat) {
     action(INIT_MOTION(LOW, view), WALK_START_MOTION(speed, view));
 
-    for (; repeat > 0; --repeat) {
+    for (; repeat > 1; --repeat) {
         RobotAction(WALK_MOTION(STEP_LEFT, speed, view));
         RobotAction(WALK_MOTION(STEP_RIGHT, speed, view));
     }
@@ -256,20 +291,40 @@ static inline void ACTION_TURN(DIRECTION dir, POSE pose, VIEW view, int repeat) 
 //  MOTION MOVE             //
 //////////////////////////////
 
-static inline void ACTION_MOVE(DIRECTION dir, POSE pose, VIEW view, int repeat) {
-    action(INIT_MOTION(pose, view), MOVE_MOTION(dir, pose, view));
+static inline void ACTION_MOVE(LENGTH len, DIRECTION dir, POSE pose, VIEW view, int repeat) {
+    action(INIT_MOTION(pose, view), MOVE_MOTION(len, dir, pose, view));
 
     for (; repeat > 1; --repeat) {
-        RobotAction(MOVE_MOTION(dir, pose, view));
+        RobotAction(MOVE_MOTION(len, dir, pose, view));
     }
 }
 
 //////////////////////////////
-//  MOTION MISSION          //
+//  MOTION BIT              //
 //////////////////////////////
 
-static inline void ACTION_MISSION(MOTION mission) {
-    action(MIDDLE_UP, mission);
+static inline void ACTION_BIT(DIRECTION dir, int repeat) {
+    action(INIT_MOTION(MIDDLE, DOWN), (dir));
+
+    for (; repeat > 1; --repeat) {
+        RobotAction(BIT_MOTION(dir));
+    }
 }
 
-#endif
+//////////////////////////////
+//  MOTION NUMBER           //
+//////////////////////////////
+
+static inline void ACTION_MOTION(MOTION mission, POSE pose, VIEW view) {
+    action(INIT_MOTION(pose, view), mission);
+}
+
+static inline void ACTION_MOTION_REPEAT(MOTION mission, POSE pose, VIEW view, int repeat) {
+    action(INIT_MOTION(pose, view), mission);
+
+    for (; repeat > 1; --repeat) {
+        RobotAction(mission);
+    }
+}
+
+#endif //SOC_APP_ROBOT_ACTION_H
